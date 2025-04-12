47abda7e109c484f9c6674f2c474209b3f448bfa
WANG <cezhewang@ust.hk>
2025-04-12 09:24:01 +0800

    Add the first version of requirements and suggestions

---
+++ utils/gradientDescent.py
@@ -0,0 +1,45 @@
+def gradientDescent(X, y, theta, alpha, num_iters):
+    """
+    Performs gradient descent to learn `theta`. Updates theta by taking `num_iters`
+    gradient steps with learning rate `alpha`.
+    
+    Parameters
+    ----------
+    X : array_like
+        The input dataset of shape (m x n+1).
+    
+    y : array_like
+        Value at given features. A vector of shape (m, ).
+    
+    theta : array_like
+        Initial values for the linear regression parameters. 
+        A vector of shape (n+1, ).
+    
+    alpha : float
+        The learning rate.
+    
+    num_iters : int
+        The number of iterations for gradient descent. 
+    
+    Returns
+    -------
+    theta : array_like
+        The learned linear regression parameters. A vector of shape (n+1, ).
+    
+    J_history : list
+        A python list for the values of the cost function after each iteration.
+    
+    Instructions
+    ------------
+    Peform a single gradient step on the parameter vector theta.
+
+    While debugging, it can be useful to print out the values of 
+    the cost function (computeCost) and gradient here.
+    """
+    # ==================== YOUR CODE HERE =================================
+        
+
+    # =====================================================================
+        
+    
+    return theta, J_history
